b069b3544a34e2af79f2dbf96b0d08cc6e407810
ryo watanabe <ryo.watanabe@theport.jp>
2025-07-14 09:57:51 +0900

    7/14

--- ui/src/main/java/com/example/ui/MainActivity.kt
+++ ui/src/main/java/com/example/ui/MainActivity.kt
@@ -3,14 +3,36 @@ package jp.co.yumemi.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hello world!!")
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally) {
+                    Row {
+                        Text("text")
+                        Text("text")
+                    }
+                    Row {
+                        Button(onClick = { /*TODO*/ }) {
+
+                        }
+                        Button(onClick = { /*TODO*/ }) {
+
+                        }
+                    }
+            }
         }
     }
 }
