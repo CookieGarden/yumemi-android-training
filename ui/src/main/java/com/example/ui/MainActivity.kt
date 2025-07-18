be6824b0957a36db15b22849ae4392236abb88ae
ryo watanabe <ryo.watanabe@theport.jp>
2025-07-18 16:24:07 +0900

    Composeを配置

--- ui/src/main/java/com/example/ui/MainActivity.kt
+++ ui/src/main/java/com/example/ui/MainActivity.kt
@@ -3,24 +3,33 @@ package jp.co.yumemi.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally) {
+            Scaffold { padding ->
+                Column(
+                    modifier = Modifier
+                        .padding(padding)
+                        .fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
                     Row {
+                        Image(painter = painterResource(id = ), contentDescription = )
                         Text("text")
                         Text("text")
                     }
@@ -32,6 +41,7 @@ class MainActivity : ComponentActivity() {
 
                         }
                     }
+                }
             }
         }
     }
